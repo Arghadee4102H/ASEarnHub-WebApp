912270d9cc9a9104600829a5cee5701b44e820e9
Arghadee4102H <arghadeepg2006@gmail.com>
2025-08-22 19:37:14 +0530

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+// firebaseConfig.js
+
+// Replace with your actual Firebase project configuration
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID" // Optional
+};
+
+// Initialize Firebase (will be called in script.js)
+// Make this object globally accessible
+window.firebaseConfig = firebaseConfig;

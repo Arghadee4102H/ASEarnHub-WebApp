0338a125ebe6f7055d358114b83d192488ed308f
Arghadee4102H <arghadeepg2006@gmail.com>
2025-08-22 19:38:48 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,13 +2,13 @@
 
 // Replace with your actual Firebase project configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
-    measurementId: "YOUR_MEASUREMENT_ID" // Optional
+    apiKey: "AIzaSyCqPOgc9VQ_y-KUujFcyCZKDlylqWzqcQ0",
+    authDomain: "as-earn-hub-e70f7.firebaseapp.com",
+    projectId: "as-earn-hub-e70f7",
+    storageBucket: "as-earn-hub-e70f7.firebasestorage.app",
+    messagingSenderId: "880785939134",
+    appId: "1:880785939134:web:7af830b581e3001c4e5cef",
+    measurementId: "G-62KBLYSCVG" // Optional
 };
 
 // Initialize Firebase (will be called in script.js)
